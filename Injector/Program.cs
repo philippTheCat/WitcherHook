6727a01f0cd3cb63058dc34237032f39dc705083
PhilippTheCat <philipp+git@philipparnosti.ch>
2018-11-25 17:47:47 +0100

    init

---
+++ Injector/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Injector {
+    internal class Program {
+        public static void Main(string[] args) {
+            string injectionLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "..\\..\\..\\WitcherHook\\bin\\Debug\\WitcherHook.dll");
+
+            Debug.WriteLine("library "+injectionLibrary);
+            Int32 targetPID = 0;
+
+            string channelName = null;
+            
+            EasyHook.RemoteHooking.IpcCreateServer<ServerInterface>(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton);
+
+            EasyHook.RemoteHooking.CreateAndInject(
+                                                   "C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Witcher 3\\bin\\x64\\witcher3.exe", // executable to run
+                                                   "", // command line arguments for target
+                                                   0, // additional process creation flags to pass to CreateProcess
+                                                   EasyHook.InjectionOptions
+                                                           .DoNotRequireStrongName, // allow injectionLibrary to be unsigned
+                                                   injectionLibrary, // 32-bit library to inject (if target is 32-bit)
+                                                   injectionLibrary, // 64-bit library to inject (if target is 64-bit)
+                                                   out targetPID, // retrieve the newly created process ID
+                                                   channelName // the parameters to pass into injected library
+                                                  );
+            // ...
+            
+            Console.WriteLine("<Press any key to exit>");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+
+    public class ServerInterface : MarshalByRefObject
+    {
+        public void IsInstalled(int clientPID)
+        {
+            Console.WriteLine("FileMonitor has injected FileMonitorHook into process {0}.\r\n", clientPID);
+        }
+
+        /// <summary>
+        /// Output the message to the console.
+        /// </summary>
+        /// <param name="fileNames"></param>
+        public void ReportMessages(string[] messages)
+        {
+            for (int i = 0; i < messages.Length; i++)
+            {
+                Console.WriteLine(messages[i]);
+            }
+        }
+
+        public void ReportMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Report exception
+        /// </summary>
+        /// <param name="e"></param>
+        public void ReportException(Exception e)
+        {
+            Console.WriteLine("The target process has reported an error:\r\n" + e.ToString());
+        }
+
+        int count = 0;
+        /// <summary>
+        /// Called to confirm that the IPC channel is still open / host application has not closed
+        /// </summary>
+        public void Ping()
+        {
+        }
+    }
+
+}
---
+++ WitcherHook/InjectionEntryPoint.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using EasyHook;
+using Injector;
+
+namespace WitcherHook {
+    public class InjectionEntryPoint: EasyHook.IEntryPoint
+    {
+        
+        ServerInterface _server = null;
+        private IntPtr factUpdateOrigPtr;
+
+
+        public InjectionEntryPoint(
+            EasyHook.RemoteHooking.IContext context,
+            string channelName) {
+            _server = EasyHook.RemoteHooking.IpcConnectClient<ServerInterface>(channelName);
+
+            // If Ping fails then the Run method will be not be called
+            _server.Ping();
+            
+        }
+        
+        public void Run(
+            RemoteHooking.IContext context,
+            string channelName) {
+            _server.IsInstalled(RemoteHooking.GetCurrentProcessId());
+
+            
+            // 4c 89 4c 24 20 44 89 44 24 18 55 57 41
+            
+            factUpdateOrigPtr = Process.GetCurrentProcess().MainModule.BaseAddress+0x7b45f0;
+            _server.ReportMessage(Process.GetCurrentProcess().MainModule.FileName);
+            _server.ReportMessage(Process.GetCurrentProcess().MainModule.BaseAddress.ToString());
+            _server.ReportMessage((Process.GetCurrentProcess().MainModule.BaseAddress+0x7b45f0).ToString());
+            
+            var hook = LocalHook.Create(factUpdateOrigPtr, new FactUpdateHook(FactUpdate_Hooked), this);
+            
+            
+            hook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+            RemoteHooking.WakeUpProcess();
+
+            
+            _server.ReportMessage("done hooking");
+            
+            try
+            {
+                // Loop until FileMonitor closes (i.e. IPC fails)
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(500);
+
+                   
+                        _server.Ping();
+                    
+                }
+            }
+            catch
+            {
+                // Ping() or ReportMessages() will raise an exception if host is unreachable
+            }
+
+            // Remove hooks
+            hook.Dispose();
+
+            // Finalise cleanup of hooks
+            EasyHook.LocalHook.Release();
+        }
+        
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.U8)]
+        delegate UInt64 FactUpdateHook(
+            Int64 unknown,
+            IntPtr factNamePtr,
+            Int32 factValue,
+            Int64 unknown2,
+            Int64 validity);
+
+        UInt64 FactUpdate_Hooked(
+            Int64 unknown,
+            IntPtr factNamePtr,
+            Int32 factValue,
+            Int64 unknown2,
+            Int64 validity) {
+
+            FactUpdateHook update = Marshal.GetDelegateForFunctionPointer<FactUpdateHook>(factUpdateOrigPtr);
+
+            int len = Marshal.ReadInt32(factNamePtr + 8);
+            _server.ReportMessage("factNamePtr => " + factNamePtr);
+            _server.ReportMessage("len => " + len);
+            String factName = Marshal.PtrToStringUni(new IntPtr(Marshal.ReadInt64(factNamePtr)), len);
+            
+            
+            _server.ReportMessage("called FactUpdate(factName=\"" + factName + "\",factValue=" + factValue + ",validity=" + validity + ")");
+            
+            
+            return update(unknown, factNamePtr, factValue, unknown2, validity);
+        }
+
+    }
+
+
+   
+}
